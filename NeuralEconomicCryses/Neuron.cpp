480480ddb0114b09f672ea32c9074445912c891b
rayman208 <rayman208@yandex.ru>
2017-05-21 12:35:14 +0300

    111

--- NeuralEconomicCryses/Neuron.cpp
+++ NeuralEconomicCryses/Neuron.cpp
@@ -10,3 +10,29 @@ Neuron::Neuron()
 Neuron::~Neuron()
 {
 }
+
+void Neuron::SetConnection(Neuron * neuron, float weight)
+{
+	m_inputConnections[neuron]=weight;
+}
+
+float Neuron::GetOutput()
+{
+	return m_output;
+}
+
+void Neuron::SetOutput(float output)
+{
+	m_output = output;
+}
+
+void Neuron::Activate()
+{
+	float net = 0;
+	for (auto & pair : m_inputConnections)
+	{
+		net += pair.first->GetOutput()*pair.second;
+	}
+
+	m_output = 1.0 / (1.0 + exp(-net));
+}
--- NeuralEconomicCryses/main.cpp
+++ NeuralEconomicCryses/main.cpp
@@ -1,12 +1,36 @@
 #include "Neuron.h"
-
+#include <vector>
 
 void main()
 {
+	vector<vector<float>> tests;//война, нефть, кредит, трудоспособность
+	vector<float> answers;
+
+	tests.push_back(vector<float>() = { 1,0,0,1 });
+	answers.push_back(1);
+
+	tests.push_back(vector<float>() = { 1,0,1,0 });
+	answers.push_back(1);
+
+	tests.push_back(vector<float>() = { 0,0,1,0 });
+	answers.push_back(0);
+
+	tests.push_back(vector<float>() = { 0,0,0,1 });
+	answers.push_back(0);
+
+	tests.push_back(vector<float>() = { 1,1,0,0 });
+	answers.push_back(1);
+
+	tests.push_back(vector<float>() = { 0,1,1,0 });
+	answers.push_back(1);
+
+
+
 	Neuron inputs[4];
 	Neuron hidden[3];
 	Neuron output;
 
 	inputs[0].SetOutput(1);
+
 	hidden[0].SetConnection(&inputs[0], 0.4);
 }
